fa36d5708c799f19e71dbf87f5f69d50838b43e8
ayalaMarmerosh <am0548505474@gmail.com>
2025-02-04 17:02:59 +0200

    LL

--- DTOs/MonthlyRecordDTO.cs
+++ DTOs/MonthlyRecordDTO.cs
@@ -2,7 +2,7 @@
 {
     public class MonthlyRecordDTO
     {
-        public int Id { get; set; }
+        public int? Id { get; set; }
         public int PersonId { get; set; }
         public string Month { get; set; }
         public string Year { get; set; }
